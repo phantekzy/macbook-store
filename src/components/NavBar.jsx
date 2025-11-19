12cb35f23c5d7ca328c73b4381d1383dd32b5f39
phantekzy <mainilotfi@gmail.com>
2025-11-19 15:07:30 +0100

    finished the navigation bar and it.s hidden in the mobile resolution

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,32 @@
+/* Import section */
+import { navLinks } from "../constants";
 /* Navigation bar component */
 export function NavBar() {
     return (
         <header>
             <nav>
                 {/* logo image */}
-                <img src="/logo.svg" />
+                <img src="/logo.svg" alt="logo" />
+                {/* Navigation lists */}
+                <ul>
+                    {
+                        navLinks.map(({ label }, key) => (
+                            <li key={key}>
+                                <a href={label}>{label}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+                <div className="flex-center gap-3">
+                    <button>
+                        <img src="/search.svg" alt="search button" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="cart button" />
+                    </button>
+                </div>
+
             </nav>
         </header>
     )
---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+/* Navigation links Array */
+export const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "iPad" },
+  { label: "iPhone" },
+  { label: "Watch" },
+  { label: "Vision" },
+  { label: "AirPods" },
+  { label: "TV & Home" },
+  { label: "Entertainment" },
+  { label: "Accessories" },
+  { label: "Contact" },
+];
