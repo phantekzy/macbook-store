84d9cc50f2b2df30e8527aff742189f2a4ef66fc
phantekzy <mainilotfi@gmail.com>
2025-11-19 14:25:28 +0100

    starting to build this Macbook air store apple like style with react and three js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* Import section */
+import { NavBar } from "./components/NavBar"
+/* App function component */
+function App() {
+    return (
+        <main>
+            <NavBar />
+        </main>
+    )
+}
+/* Export section */
+export default App
+
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+/* Navigation bar component */
+export function NavBar() {
+    return (
+        <header>
+            <nav>
+                {/* logo image */}
+                <img src="/logo.svg" />
+            </nav>
+        </header>
+    )
+}
